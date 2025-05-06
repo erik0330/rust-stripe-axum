776eeda5945cf8ce3895744f0aa9871aee1be7f4
erik0330 <erikkaryan0330@gmail.com>
2025-05-06 16:25:11 +0800

    connect database

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use async_stripe::
+use sqlx::postgres::PgPoolOptions;
+use stripe::{Client as StripeClient};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::fmt::init();
+
+    dotenv::dotenv().ok();
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL is required.");
+    let stripe_secret_key = std::env::var("STRIPE_SECRET_KEY").expect("STRIPE_SECRET_KEY is required.");
+    let stripe_webhook_secret = std::env::var("STRIPE_WEBHOOK_SECRET").expect("STRIPE_WEBHOOK_SECRET is required.");
+
+    let db_pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await?;
+    
+    let stripe_client = StripeClient::new(stripe_secret_key);
+    
 }
